7da5e0298468a61a8bb1533331f2fe44a6d1b886
ValeMontecinosP <valemontecinosp@gmail.com>
2022-04-29 13:06:01 -0400

    brekkie json

--- src/components/Breakfast.js
+++ src/components/Breakfast.js
@@ -1,31 +1,18 @@
-const breakfast = [
-    {
-        id: 1,
-        text: "Café americano $5",
-    },
-    {
-        id: 2,
-        text: "Café con leche $7",
-    },
-    {
-        id: 3,
-        text: "Sándwich de jamón y queso $10",
-    },
-    {
-        id: 4,
-        text: "Jugo de frutas natural $7",
-    }
-]
+import Button from "./Button"
+import breakfast from "../breakfast.json"
 
 const Breakfast = () => {
     return (
         <>
-        {breakfast.map((brekkie)=>(
-        <h3 key={brekkie.id}>{brekkie.text}</h3>)
-        )}
+            {breakfast.map((brekkie) => (
+                <Button text={`${brekkie.text} $${brekkie.value}`} />
+            )
+            )}
 
         </>
 
     )
 }
 export default Breakfast
+
+//onClick as a prop
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types"
+import Button from "./Button"
 
 const Header = ({title}) => {
     return (
         <header>
+            <h5>Tiene un costo monetario de</h5>
             <h1>{title}</h1>
+            <Button text="Desayuno"/>
+            <Button text="Menú diario"/>
         </header>
     )
 }
 Header.defaultProps = {
-    title: "Burger Queen",
+    title: "Sinco peso",
 }
 Header.propTypes = {
     title: PropTypes.string,
